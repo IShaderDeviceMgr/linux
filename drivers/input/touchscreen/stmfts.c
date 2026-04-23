7b81536f3ece6ef00c99124116823ffb25c915f0
IShaderDeviceMgr <49534369+IShaderDeviceMgr@users.noreply.github.com>
2026-04-22 17:32:53 -0700

    Work on touchscreen, start working on battery

--- drivers/input/touchscreen/stmfts.c
+++ drivers/input/touchscreen/stmfts.c
@@ -6,6 +6,7 @@
 
 #include <linux/delay.h>
 #include <linux/i2c.h>
+#include <linux/gpio.h>
 #include <linux/input/mt.h>
 #include <linux/input/touchscreen.h>
 #include <linux/interrupt.h>
@@ -621,6 +622,8 @@ static int stmfts_probe(struct i2c_client *client)
 {
 	int err;
 	struct stmfts_data *sdata;
+
+	dev_warn(dev, "LFRYZEK Testing modified stmfts\n");
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C |
 						I2C_FUNC_SMBUS_BYTE_DATA |
